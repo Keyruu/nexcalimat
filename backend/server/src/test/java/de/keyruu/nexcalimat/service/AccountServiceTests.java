1e4d725f6530d3d4d2d45465e37962f34e128d95
nag2548 <nadine.grabmair@gmx.de>
2022-11-26 00:50:21 +0100

    ✅ Add pin test ^^

---
+++ backend/server/src/test/java/de/keyruu/nexcalimat/service/AccountServiceTests.java
@@ -0,0 +1,31 @@
+package de.keyruu.nexcalimat.service;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class AccountServiceTests {
+    @ParameterizedTest
+    @ValueSource(strings = { "1234", "0123", "6969" })
+    void testValidPins(String pin) {
+        // given
+        // when
+        boolean isNumeric = AccountService.isNumeric(pin);
+
+        // then
+        assertTrue(isNumeric);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "abcd", "0,56", "0.345", "1.456" })
+    void testInvalidPins(String pin) {
+        // given
+        // when
+        boolean isNumeric = AccountService.isNumeric(pin);
+
+        // then
+        assertFalse(isNumeric);
+    }
+}
