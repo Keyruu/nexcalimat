8c827ca9fb1b5a4d13b8afd76126fd019b0c4420
Lucas <keyruu@web.de>
2023-01-11 17:01:45 +0100

    🚧 add authjs and try azuread

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,10 +1,25 @@
 import type { Handle } from '@sveltejs/kit';
 import { locale } from 'svelte-i18n';
+import { sequence } from '@sveltejs/kit/hooks';
+import { SvelteKitAuth } from "@auth/sveltekit";
+import AzureADProvider from "@auth/core/providers/azure-ad";
 
-export const handle: Handle = async ({ event, resolve }) => {
+const i18n: Handle = async ({ event, resolve }) => {
 	const lang = event.request.headers.get('accept-language')?.split(',')[0];
 	if (lang) {
 		locale.set(lang);
 	}
+
 	return resolve(event);
 };
+
+export const handle: Handle = sequence(
+	SvelteKitAuth({
+		providers: [AzureADProvider({
+			clientId: process.env.AZURE_AD_CLIENT_ID,
+			clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+			tenantId: process.env.AZURE_AD_TENANT_ID,
+		}),]
+	}),
+	i18n
+);
